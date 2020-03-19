6415b2d6e29a420179ba238aad1db3d0a410d030
Alice Mouchard <alice.mouchard85@gmail.com>
2020-03-19 11:15:25 +0100

    Setup

---
+++ js/app.js
@@ -0,0 +1,30 @@
+/*
+Attendre le chargement du DOM
+*/
+document.addEventListener('DOMContentLoaded', () => {
+
+  /*
+  Déclarations
+  */
+      const apiUrl = 'https://newsapp.dwsapp.io/api';
+      const searchSourceForm = document.querySelector('#searchSourceForm');
+      const searchKeywordForm = document.querySelector('#searchKeywordForm');
+      const searchSourceData = document.querySelector('[name="searchSourceData"]');
+      const searchKeywordData = document.querySelector('[name="searchKeywordData"]');
+      const newsList = document.querySelector('#newsList');
+  //
+
+  /*
+  Fonctions
+  */
+
+  //
+
+  /*
+  Lancer IHM
+  */
+      /*
+      Start interface by checkingg if user token is prersent
+      */
+  //
+});
